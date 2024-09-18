805b17daf7e0274ecc953d97aa73e478806a5a24
Farouk Brown <aionoid.xd@gmail.com>
2024-09-18 12:01:56 +0100

    :lipstick: Set dark theme for greeted

--- hosts/hyprland.nix
+++ hosts/hyprland.nix
@@ -18,6 +18,14 @@
   # INFO: we can use sddm+theme
   # using regreet for now better than regreetd
   programs.regreet.enable = true;
+  programs.regreet.settings = {
+    GTK.application_prefer_dark_theme = true;
+    # GTK.theme_name = "Gtk";
+    # GTK.cursor_theme_name = "Bibata-Modern-Ice";
+    # GTK.font_name = "JetBrainsMono Nerd Font Mono";
+    appearance.greeting_msg = "HELLO BOSS";
+    backgound.path = "${pkgs.gnome-backgrounds}/share/backgrounds/gnome/morphogenesis-d.svg";
+  };
   # services.regreetd = {
   #   enable = true;
   #   settings = {
