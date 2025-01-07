8b626fa6095fbb3a51d5ce30cb80f8cc915c35f4
Farouk Brown <aionoid.xd@gmail.com>
2025-01-07 23:34:21 +0100

    :bug: Fix screen for tv

--- home-manager/ovo.nix
+++ home-manager/ovo.nix
@@ -14,9 +14,9 @@
       monitor = [
         # :INFO: right monitor
         # "HDMI-A-1,1600x900,1440x0,1.2"
-        "HDMI-A-1,1600x900,0x0,1.2"
+        "HDMI-A-1,1920x1080,0x0,1"
         # :INFO: left monitor
-        "DP-2,1440x900,1600x0,1"
+        "DP-2,1440x900,1920x0,1"
         # monitor = HDMI-A-1,1920x1080@60,1920x0,1
         ",preferred,auto,1"
       ];
