195b0addd8999ff7d4ce57eef8c13a5094707dd4
Farouk Brown <aionoid.xd@gmail.com>
2025-02-16 16:17:42 +0100

    :construction: Patch script for ip address

---
+++ hosts/containers/scripts/patch.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/env bash
+
+# Function to convert an IP address to hex (first three octets)
+ip_to_hex() {
+    local ip="$1"
+    IFS='.' read -r o1 o2 o3 _ <<< "$ip"
+    server_ip="$o1.$o2.$o3.0"
+
+    hex_ip=""
+
+    for char in $(echo "$server_ip" | grep -o .); do
+      hex_ip+=$(printf '%02x' "'$char")
+    done
+
+    while [ ${#hex_ip} -lt 30 ]; do
+      hex_ip+="00"
+    done
+    printf "%s" "$hex_ip"
+}
+
+ip_to_hex_local() {
+    local ip="$1"
+    hex_ip_local=""
+    IFS='.' read -r o1 o2 _ _ <<< "$ip"
+
+    server_ip_local="$o1.$o2."
+
+    for char in $(echo "$server_ip_local" | grep -o .); do
+      hex_ip_local+=$(printf '%02x' "'$char")
+    done
+
+    while [ ${#hex_ip_local} -lt 16 ]; do
+      hex_ip_local+="00"
+    done
+    printf "%s" "$hex_ip_local"
+}
+
+# example of a nix attribute set in json 
+# {
+#  "WorldServer": {
+#    "global": [
+#      "0x3E2C27" # Offset
+#    ]
+#  },
+#  "ZoneServer": {
+#    "global": [
+#      "0x14D1FA",
+#      "0x152007"
+#    ],
+#    "local": [
+#      "0x16AE07"
+#    ]
+#  }
+# }
+
+# Main script
+main() {
+    local ip="$1"
+    # local servers="$2"
+    # TODO: change back to input and use builtins.toJSON in the nix script
+    local servers="{\"WorldServer\":{\"global\":[\"0x3E2C27\"]},\"ZoneServer\":{\"global\":[\"0x14D1FA\",\"0x152007\"],\"local\":[\"0x16AE07\"]}}"
+
+    clear
+    # Convert the first three octets of the IP to hex
+    local hex_ip
+    local hex_ip_local
+
+    hex_ip=$(ip_to_hex "$ip")
+    hex_ip_local=$(ip_to_hex_local "$ip")
+
+    local ip_bytes
+    local ip_local_bytes
+
+    ip_bytes=$(echo "$hex_ip" | sed 's/\(..\)/\\\x\1/g')
+    ip_local_bytes=$(echo "$hex_ip_local" | sed 's/\(..\)/\\\x\1/g')
+
+    ##################3
+    # Iterate over the servers and their offset types (local/global)
+    for server in $(echo "$servers" | jq -r 'keys[]'); do
+        echo "Patching $server..."
+
+        # Patch local offsets
+        local local_offsets
+        local_offsets=$(echo "$servers" | jq -r ".$server.local[]?")
+        for offset in $local_offsets; do
+            echo "  Patching LOCAL  IP offset $offset with $hex_ip_local..."
+            # echo -n "$hex_local" | xxd -r -p | dd of="$server" bs=1 seek="$((offset))" conv=notrunc
+            echo -en "$ip_bytes" | dd of="${server}/${server}" bs=1 seek=$((0x$offset)) count=${#ip_local_bytes} conv=notrunc >/dev/null 2>&1
+        done
+
+        # Patch global offsets
+        local global_offsets
+        global_offsets=$(echo "$servers" | jq -r ".$server.global[]?")
+        for offset in $global_offsets; do
+            echo "  Patching GLOBAL IP offset $offset with $hex_ip..."
+            # echo -n "$hex_global" | xxd -r -p | dd of="$server" bs=1 seek="$((offset))" conv=notrunc
+            echo -en "$ip_bytes" | dd of="${server}/${server}" bs=1 seek=$((0x$offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+        done
+    done
+    #
+    # echo -en "$ip_bytes" | dd of="${gateway_fb}/${gateway_fb}" bs=1 seek=$((0x$gateway_offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+    # echo -en "$ip_bytes" | dd of="${login_fb}/${login_fb}" bs=1 seek=$((0x$login_offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+    # echo -en "$ip_bytes" | dd of="${mission_fb}/${mission_fb}" bs=1 seek=$((0x$mission_offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+    # echo -en "$ip_bytes" | dd of="${ticket_fb}/${ticket_fb}" bs=1 seek=$((0x$ticket_offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+    # echo -en "$ip_bytes" | dd of="${bin_folder}/${world_bin}" bs=1 seek=$((0x$world_offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+    # echo -en "$ip_bytes" | dd of="${bin_folder}/${zone_bin}" bs=1 seek=$((0x$zone_offset)) count=${#ip_bytes} conv=notrunc >/dev/null 2>&1
+    #
+    # cp -f ${bin_folder}/${world_bin} ${world101_fb}/${world101_fb}
+    # cp -f ${bin_folder}/${world_bin} ${world102_fb}/${world102_fb}
+    # cp -f ${bin_folder}/${world_bin} ${world109_fb}/${world109_fb}
+    # cp -f ${bin_folder}/${zone_bin} ${zone101_fb}/${zone101_fb}
+    # cp -f ${bin_folder}/${zone_bin} ${zone102_fb}/${zone102_fb}
+    # cp -f ${bin_folder}/${zone_bin} ${zone109_fb}/${zone109_fb}
+
+    echo "Patching complete!"
+}
+
+# Run the script
+main "$@"
+
