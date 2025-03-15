53271f3b44e20eb2ce63c7be938d01e57684f575
Farouk Brown <aionoid.xd@gmail.com>
2025-03-15 01:53:03 +0100

    :sparkles: Add hyprsplit input in flake for hyprland

--- flake.nix
+++ flake.nix
@@ -44,6 +44,11 @@
       url = "github:Duckonaut/split-monitor-workspaces";
       inputs.hyprland.follows = "hyprland"; # <- make sure this line is present for the plugin to work as intended
     };
+
+    hyprsplit = {
+      url = "github:shezdy/hyprsplit";
+      inputs.hyprland.follows = "hyprland"; # <- make sure this line is present for the plugin to work as intended
+    };
   };
 
   outputs = {
--- modules/home-manager/hyprland/plugins.nix
+++ modules/home-manager/hyprland/plugins.nix
@@ -5,31 +5,33 @@
 }: {
   wayland.windowManager.hyprland = {
     plugins = [
-      #   inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
+      inputs.hyprland-plugins.packages.${pkgs.system}.borders-plus-plus
       inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
+      # inputs.hyprsplit.packages.${pkgs.system}.hyprsplit
     ];
     #
     settings = {
       plugin = {
+        split-monitor-workspaces = {
+          count = 10;
+        };
+        # hyprsplit = {
+        #   num_workspaces = 10;
+        # };
         borders-plus-plus = {
-          count = 6;
+          count = 2;
+          # you can add up to 9 borders
+          "col.border_1" = "rgb(ffffff)";
+          "col.border_2" = "rgb(2222ff)";
+
+          # -1 means "default" as in the one defined in general:border_size
+          border_size_1 = 10;
+          border_size_2 = -1;
+
+          # makes outer edges match rounding of the parent. Turn on / off to better understand. Default = on.
+          natural_rounding = "yes";
         };
       };
     };
-    #   "plugin:borders-plus-plus" = {
-    #     add_borders = 1; # 0 - 9
-    #
-    #     # you can add up to 9 borders
-    #     "col.border_1" = "rgb(ffffff)";
-    #     "col.border_2" = "rgb(2222ff)";
-    #
-    #     # -1 means "default" as in the one defined in general:border_size
-    #     border_size_1 = 10;
-    #     border_size_2 = -1;
-    #
-    #     # makes outer edges match rounding of the parent. Turn on / off to better understand. Default = on.
-    #     natural_rounding = "yes";
-    #   };
-    #
   };
 }
