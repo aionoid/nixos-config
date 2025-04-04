cdbb3849766151a0453ff563e92cfc9231cb870b
Farouk Brown <aionoid.xd@gmail.com>
2025-04-04 11:14:48 +0100

    :wrench: Add ollama

--- modules/nixos/ollama.nix
+++ modules/nixos/ollama.nix
@@ -1,7 +1,6 @@
 {inputs, ...}: {
   services.ollama = {
     enable = true;
-    # # FIXME: use future nixpkgs version unstable
     # package = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.ollama;
   };
 }
