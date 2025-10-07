047eabbb9b82d3c86d94111e585b8fb463220dea
Farouk Brown <aionoid.xd@gmail.com>
2025-10-07 10:25:19 +0100

    🔧 Avante: use llama-cpp for ai

--- modules/home-manager/nvf/avante.nix
+++ modules/home-manager/nvf/avante.nix
@@ -5,24 +5,29 @@
         avante-nvim = {
           enable = true;
           setupOpts = {
-            provider = "ollama";
-            # for v0.0.24^
+            debug = false;
+            provider = "llama_cpp";
             providers = {
-              ollama = {
-                model = "qwen25coder:7bq4kl";
+              llama_cpp = {
+                __inherited_from = "openai";
+                endpoint = "http://127.0.0.1:8080/v1";
+                # model = "GLM-4-32B-0414-UD-Q4_K_XL.gguf";
+                # model = "GLM-4-9B-0414-Q4_K_M.gguf";
+                # model = "GLM-4-9B-0414-UD-Q4_K_XL.gguf";
+                model = "Qwen3-4B-Instruct-2507-UD-Q4_K_XL.gguf";
+                api_key_name = "";
+                stream = true;
               };
-              glm4 = {
-                __inherited_from = "ollama";
-                model = "glm4:q4xl";
-              };
-              qwen3coder = {
-                __inherited_from = "ollama";
-                model = "qwen3coder:q4xl";
+              lmstudio = {
+                __inherited_from = "openai";
+                endpoint = "http://127.0.0.1:1234/v1";
+                # model = "glm-4/GLM-4-9B-0414-Q4_K_M.gguf";
+                # model = "qwen2.5";
+                model = "unsloth/qwen3/qwen3-4b-instruct-2507-ud-q4_k_xl.gguf";
+                api_key_name = "";
+                stream = true;
               };
             };
-            # qwen_nvim = {
-            #   model = "qwen2.5-coder-ins-unc:nvim";
-            # };
           };
         };
       };
