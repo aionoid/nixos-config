b503f08ee8f562883703764b6d577f98bf5690ff
Farouk Brown <aionoid.xd@gmail.com>
2026-01-06 21:55:22 +0100

    ♻️ Add virt-manager app to vm

--- hosts/vm.nix
+++ hosts/vm.nix
@@ -6,6 +6,7 @@
     # Enable TPM emulation (for Windows 11)
     qemu = {
       swtpm.enable = true;
+      vhostUserPackages = with pkgs; [virtiofsd];
       # ovmf.packages = [pkgs.OVMFFull.fd];
     };
   };
@@ -16,6 +17,12 @@
   # Allow VM management
   users.groups.libvirtd.members = ["ovo" "antiroot"];
   users.groups.kvm.members = ["ovo" "antiroot"];
+
+  programs.virt-manager.enable = true;
+
+  # When running NixOS as a guest
+  services.qemuGuest.enable = true;
+  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
 
   # Enable VM networking and file sharing
   environment.systemPackages = with pkgs; [
